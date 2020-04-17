2c6e5b12aa7e764f9724c600539701b0457665f8
DillTurner <54756447+DillTurner@users.noreply.github.com>
2020-04-17 15:52:03 -0700

    fixed

---
+++ script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    var time = moment().local().format("dddd, MMMM Do YYYY");
+    
+    $('p').text(time);
+    renderText();
+    //var timeMatch =  moment().hour();
+    //console.log(timeMatch);
+    $('form').submit("submit",function(e){
+        e.preventDefault();
+        $('input').empty();
+        var nineText = $("#9-text").val();
+        var tenText = $('#ten-am').val();
+        var elevenText= $('#eleven-am').val();
+        var twelveText= $('#twelve-pm').val();
+        var oneText= $('#one-pm').val();
+        var twoText= $('#two-pm').val();
+        var threeText= $('#three-pm').val();
+        var fourText= $('#four-pm').val();
+        var fiveText= $('#five-pm').val();
+        var schedule = {
+            nine: nineText.trim(),
+            ten: tenText.trim(),
+            eleven: elevenText.trim(),
+            twelve: twelveText.trim(),
+            one: oneText.trim(),
+            two: twoText.trim(),
+            three: threeText.trim(),
+            four: fourText.trim(),
+            five: fiveText.trim(),
+        };
+        //console.log(schedule);
+        console.log(event.type);
+        localStorage.setItem("schedule",JSON.stringify(schedule));
+        
+        renderText();
+    })
+            function renderText(){
+                var sched = JSON.parse(localStorage.getItem('schedule'));
+                if(sched === null){
+                    return;
+                }
+                else{
+                 $("#9-text").val(sched.nine);
+                 $('#ten-am').val(sched.ten);
+                 $('#eleven-am').val(sched.eleven);
+                 $('#twelve-pm').val(sched.twelve);
+                 $('#one-pm').val(sched.one);
+                 $('#two-pm').val(sched.two);
+                 $('#three-pm').val(sched.three);
+                 $('#four-pm').val(sched.four);
+                 $('#five-pm').val(sched.five);
+                console.log(sched);}
+            }
+ 
+});
